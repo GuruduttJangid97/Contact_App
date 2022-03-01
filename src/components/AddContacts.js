60c9bd990e8b50fc4fb867ffbd4ee6c49d69e927
guruduttjangid97 <sharmagurudutt9950@hotmail.com>
2022-03-01 11:19:46 -0800

    Files added

---
+++ src/components/AddContacts.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const x = React.Component;
+
+class AddContact extends x {
+    render(){
+        return(
+            <div className='ui main'>
+                <h2>AddContact</h2>
+                <form className='ui form'>
+                    <div className='field'>
+                        <label>Name</label>
+                        <input type="Text" name="name" placeholder='name'/>
+                     </div>
+                    <div className='field'>
+                        <label>Name</label>
+                        <input type="Text" name="name" placeholder='name'/>
+                     </div>
+                    </form>
+            </div>
+        );
+    }
+}
