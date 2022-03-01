69493e1e4ef76143ef4d13353ffb5245e0759700
guruduttjangid97 <sharmagurudutt9950@hotmail.com>
2022-03-01 11:02:18 -0800

    Files created

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+import AddContact from './AddContacts';
+import ContacTlist from './Contact-list'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <AddContact/>
+      <ContacTlist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+    return(
+        <div>
+            <div className='ui fixed menu'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
