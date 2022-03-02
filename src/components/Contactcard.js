b2d4c425c41d27b4e0edf22610b26add9888721d
guruduttjangid97 <sharmagurudutt9950@hotmail.com>
2022-03-02 06:43:45 -0800

    UI Avatar Added

--- src/components/Contactcard.js
+++ src/components/Contactcard.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import user from './images/user.png'
 
 const CardContact = (props) =>{
     const {id, name, email} = props.contacts;
     return(
-      <div className='item'>
-        <div className='content'>
-          <div className='header'>{name}</div>
+      <div className="item">
+        <img className="ui avatar image" src={user} alt="user" />
+        <div className="content">
+          <div className="header">{name}</div>
           <div>{email}</div>
         </div>
         <i className='trash alternate outline icon' style={{color: "red", marginTop: "7px"}}></i>
